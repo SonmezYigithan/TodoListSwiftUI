96567573ed853cac9c2570fbfd469248dd70c6e0
SonmezYigithan <yigithan.sonmez@mildmania.com>
2023-06-18 14:06:32 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Yiğithan Sönmez on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
